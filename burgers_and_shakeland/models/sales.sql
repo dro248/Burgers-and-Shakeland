026de4e5032c7d642080fcd79a3fb0aa87af2e76
David <david.ostler001@gmail.com>
2022-09-03 02:21:46 -0700

    updatesdocs

--- burgers_and_shakeland/models/sales.sql
+++ burgers_and_shakeland/models/sales.sql
@@ -1,12 +1,19 @@
 
 {{ config(materialized='table') }}
 
+with renamed as (
+	select
+		"Transaction EmployeeID"	                     as "employee_id",
+		"Employee Name"				                     as "employee_name",
+		"ItemID" 					                     as "item_id",
+		"Item"						                     as "item",
+		"Price" 					                     as "price",
+		"Location"					                     as "location",
+		("Date"::date || ' ' || "Time"::time)::timestamp as ts
+	from {{ ref('sales_raw') }}
+)
+
 select
-	"Transaction EmployeeID"	                     as "employee_id",
-	"Employee Name"				                     as "employee_name",
-	"ItemID" 					                     as "item_id",
-	"Item"						                     as "item",
-	"Price" 					                     as "price",
-	"Location"					                     as "location",
-	("Date"::date || ' ' || "Time"::time)::timestamp as ts
-from {{ ref('sales_raw') }}
+	md5(concat(ts, employee_id, location)) as transaction_id,
+	*
+from renamed
--- burgers_and_shakeland/target/compiled/burgers_and_shakeland/models/sales.sql
+++ burgers_and_shakeland/target/compiled/burgers_and_shakeland/models/sales.sql
@@ -1,11 +1,18 @@
 
 
+with renamed as (
+	select
+		"Transaction EmployeeID"	                     as "employee_id",
+		"Employee Name"				                     as "employee_name",
+		"ItemID" 					                     as "item_id",
+		"Item"						                     as "item",
+		"Price" 					                     as "price",
+		"Location"					                     as "location",
+		("Date"::date || ' ' || "Time"::time)::timestamp as ts
+	from "postgres"."burgers_and_shakeland"."sales_raw"
+)
+
 select
-	"Transaction EmployeeID"	                     as "employee_id",
-	"Employee Name"				                     as "employee_name",
-	"ItemID" 					                     as "item_id",
-	"Item"						                     as "item",
-	"Price" 					                     as "price",
-	"Location"					                     as "location",
-	("Date"::date || ' ' || "Time"::time)::timestamp as ts
-from "postgres"."burgers_and_shakeland"."sales_raw"
+	md5(concat(ts, employee_id, location)) as transaction_id,
+	*
+from renamed
