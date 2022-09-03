037303784212c93890a476fc0df693ec0eecfab7
David <david.ostler001@gmail.com>
2022-09-02 18:24:24 -0700

    Adds docs

---
+++ burgers_and_shakeland/target/compiled/burgers_and_shakeland/models/sales.sql
@@ -0,0 +1,11 @@
+
+
+select
+	"Transaction EmployeeID"	                     as "employee_id",
+	"Employee Name"				                     as "employee_name",
+	"ItemID" 					                     as "item_id",
+	"Item"						                     as "item",
+	"Price" 					                     as "price",
+	"Location"					                     as "location",
+	("Date"::date || ' ' || "Time"::time)::timestamp as ts
+from "postgres"."burgers_and_shakeland"."sales_raw"
